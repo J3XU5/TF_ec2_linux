2ea3dd71ec833bfb7e3e27df9592eae7b44e6cc5
J3XU5 <hravard@hotmail.fr>
2023-11-25 10:49:56 +0100

    Add tag name

--- instance.tf
+++ instance.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "ec2_instance" {
-    ami = var.ami
-
-    instance_type = var.type
-
-    
+  ami           = var.ami
+  instance_type = var.type
+  
+  tags = {
+    Name = var.instance_name
+  }
 }
--- var.tf
+++ var.tf
@@ -1,9 +1,12 @@
-variable "realm"{
-    default = ""
+variable "realm" {
+  default = ""
 }
-variable "ami"{
-    default = "ami-0302f42a44bf53a45"
+variable "ami" {
+  default = "ami-0302f42a44bf53a45"
 }
-variable "type"{
-    default = ""
+variable "type" {
+  default = ""
+}
+variable "instance_name" {
+  default = "instance_name"
 }
